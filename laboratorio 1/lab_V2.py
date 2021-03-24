cf88a8c204b6fd66fa6f6997678b411e3f6d966d
jmcaussade <jmcaussade@miuandes.cl>
2021-03-23 21:39:23 -0300

    Primera parte lista

--- laboratorio 1/lab_V2.py
+++ laboratorio 1/lab_V2.py
@@ -1,1 +1,16 @@
-print(hola)
+from numpy import random
+#parte 1
+Numero_Cartas=int(input("Ingresa el número de cartas: "))
+Lista1=[]
+Lista2=[]
+i=0
+r=1
+while i<Numero_Cartas:
+    Lista1.append(r)            #falta borrar los prints
+    Lista2.append(r)
+    r+=1
+    i+=1
+print(Numero_Cartas)
+print(Lista1)
+print(Lista2)
+
