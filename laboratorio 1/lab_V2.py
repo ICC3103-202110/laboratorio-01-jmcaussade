9dc27eb676fcb98708f5c9f5eddce4a26faf0b47
jmcaussade <jmcaussade@miuandes.cl>
2021-03-25 09:28:34 -0300

    segunda parte lista

--- laboratorio 1/lab_V2.py
+++ laboratorio 1/lab_V2.py
@@ -13,4 +13,28 @@ while i<Numero_Cartas:
 print(Numero_Cartas)
 print(Lista1)
 print(Lista2)
+print("#####")
+#parte 2                    
+Lista1_Random=[]
+Lista2_Random=[]
+i=0
+while i<Numero_Cartas:
+    x=random.choice(Lista1)
+    Lista1_Random.append(x)
+    Lista1.remove(x)
+    y=random.choice(Lista2)
+    Lista2_Random.append(y)
+    Lista2.remove(y)
+    i+=1
+Lista_Maestra=[]             #falta borrar los prints
+Lista_Maestra.append(Lista1_Random)
+Lista_Maestra.append(Lista2_Random)
+print("Lista 1 ",Lista1)                
+print("Lista1_Random ", Lista1_Random)
+print("Lista 2 ",Lista2)
+print("Lista2_Random ", Lista2_Random)
+print("Lista Maestra ",Lista_Maestra)
+
+
+    
 
