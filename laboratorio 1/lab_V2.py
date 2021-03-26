834f5597f385e62738b5276b6e2c2ff0fab0e1c8
jmcaussade <jmcaussade@miuandes.cl>
2021-03-26 14:25:45 -0300

    Hasta parte 7 bien

--- laboratorio 1/lab_V2.py
+++ laboratorio 1/lab_V2.py
@@ -41,38 +41,54 @@ Jugador1=0
 Jugador2=0
 
 #parte 4
-#imprimir tablero con coordenadas
-#(falta transformarlo en funcion)
-Tablero=[]
-i=0
-while i<len(Lista_Maestra):
-    r=0
-    Sub_Tablero=[]
-    while r<len(Lista_Maestra[0]):
-        Coordenadas=[]
-        Coordenadas.append(i)
-        Coordenadas.append(r)
-        Sub_Tablero.append(Coordenadas)
-        r+=1
-    Tablero.append(Sub_Tablero)
-    print(Sub_Tablero)
-    i+=1
+#Shows board with coordinates
+def Creat_Board():
+    Board=[]
+    i=0
+    while i<len(Lista_Maestra):
+        r=0
+        Sub_Board=[]
+        while r<len(Lista_Maestra[0]):
+            Coordinates=[]
+            Coordinates.append(i)
+            Coordinates.append(r)
+            Sub_Board.append(Coordinates)
+            r+=1
+        Board.append(Sub_Board)
+        print(Sub_Board)
+        i+=1
+    return Board #returns straight board
 
 #parte 5 (Asks for the card coordinates)
 def Input_Card_Coordinates():
     String=str(input("Ingresa las coordenadas te tu carta: "))
     print(String)
-    h=["(",")"]
+    h=["(",")","."]
     for h in String:
         String=String.replace("(","")
         String=String.replace(")","")
+        String=String.replace(".",",")
     print(String)
     Lista_Carta_Elegida=String.split(",")
     print(Lista_Carta_Elegida)
     return(Lista_Carta_Elegida)
+#hay que revisar de aqui en adelante
+#print(Input_Card_Coordinates())
+#print("ahora viene tablero coordenadas")
+#print(Creat_Board())
+def Show_Cards_in_Board(List_Coordinates,List_Board,List_Values):
+    x=int(List_Coordinates[0])#x coordinate
+    print("valor x",x)
+    y=int(List_Coordinates[1]) #y coordinate
+    print("valor y",y)
+    Value=List_Values[x][y]#valor al que corresponde la coordenada
+    print("value",Value,"\n") #hasta qui bien
+    List_Board[x].pop(y)
+    List_Board[x].insert(y,Value)
+    return List_Board
 
-print(Input_Card_Coordinates())
-
+print(Show_Cards_in_Board(Input_Card_Coordinates(),Creat_Board(),Lista_Maestra))
+    
 
 
 
